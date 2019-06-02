c6fbe1f646a4c76d27a05894a2e5cae345758705
Shaphil <shaphil2025@gmail.com>
2019-06-03 02:39:42 +0600

    Add GroupsController

---
+++ Controllers/GroupsController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Contacts.Models;
+
+namespace Contacts.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupsController : ControllerBase
+    {
+        private readonly ContactsContext _context;
+
+        public GroupsController(ContactsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Group>>> GetGroups()
+        {
+            return await _context.Groups.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Group>> GetGroup(int id)
+        {
+            var group = await _context.Groups.FindAsync(id);
+
+            if (group == null)
+            {
+                return NotFound();
+            }
+
+            return group;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Group>> PostGroup(Group group)
+        {
+            _context.Groups.Add(group);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetGroup), new { id = group.GroupId }, group);
+        }
+    }
+}
