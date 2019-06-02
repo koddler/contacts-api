ade0b8dee1e496d13c10476fe7bca4af9543666b
Shaphil <shaphil2025@gmail.com>
2019-06-03 02:28:10 +0600

    Register database context

--- Startup.cs
+++ Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+
+using Contacts.Models;
 
 namespace Contacts
 {
@@ -26,6 +29,7 @@ namespace Contacts
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<ContactsContext>(options => options.UseSqlite(Configuration.GetConnectionString("ContactsDatabase")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
