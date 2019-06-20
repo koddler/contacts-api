d432a484dc27f523b7105b0a1487f1b68a246450
Shaphil <shaphil2025@gmail.com>
2019-06-20 16:56:31 +0600

    Add Avatar field to model Contact

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -9,6 +9,7 @@ namespace Contacts.Models
         public string Name { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
+        public string Avatar { get; set; }
 
         public int GroupId { get; set; }
         [JsonIgnore]
