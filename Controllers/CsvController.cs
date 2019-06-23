17cd4fc20392071afdfb68dd832782284615c219
Shaphil <shaphil2025@gmail.com>
2019-06-24 05:16:01 +0600

    Set and Get current progress count to/from cache

--- Controllers/CsvController.cs
+++ Controllers/CsvController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -15,11 +17,27 @@ namespace Contacts.Controllers
     [ApiController]
     public class CsvController : ControllerBase
     {
+        private const string CACHE_KEY_COUNT = "CACHE_KEY_COUNT";
         private readonly ContactsContext _context;
+        private IMemoryCache _cache;
 
-        public CsvController(ContactsContext context)
+        public CsvController(ContactsContext context, IMemoryCache memoryCache)
         {
             _context = context;
+            _cache = memoryCache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            int count;
+            if (!_cache.TryGetValue(CACHE_KEY_COUNT, out count))
+            {
+                count = 0;
+            }
+
+            await Task.Delay(0);
+            return Ok(new { count });
         }
 
         [HttpPost]
@@ -30,11 +48,15 @@ namespace Contacts.Controllers
                 var stream = file.OpenReadStream();
                 var name = file.FileName;
 
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                        .SetSlidingExpiration(TimeSpan.FromSeconds(3));
+
                 using (var reader = new StreamReader(stream))
                 using (var csv = new CsvReader(reader))
                 {
                     csv.Configuration.RegisterClassMap<ContactMap>();
                     var contacts = csv.GetRecords<Contact>();
+                    int count = 0;
                     foreach (var contact in contacts)
                     {
                         var group = await _context.Groups.FindAsync(contact.GroupId);
@@ -48,6 +70,11 @@ namespace Contacts.Controllers
                         }
 
                         _context.Contacts.Add(contact);
+
+                        count++;
+                        _cache.Set(CACHE_KEY_COUNT, count, cacheEntryOptions);
+
+                        await Task.Delay(1000);
                     }
                     await _context.SaveChangesAsync();
                 }
